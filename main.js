6c5078a8f872d67b9dc7c35fcec801b97ad73240
geovana soare ferreira <148552073+geovanasoare@users.noreply.github.com>
2025-06-16 19:08:00 -0300

    .ç

--- main.js
+++ main.js
@@ -10,23 +10,12 @@ if (tamanhoSenha > 1){
 tamanhoSenha--;
 }
 numeroSenha.textContent = tamanhoSenha;
+geraSenha();
 }
 function aumentaTamanho(){
 if (tamanhoSenha < 20){
 /*tamanhoSenha = tamanhoSenha+1;*/
 tamanhoSenha++;
-}
-numeroSenha.textContent = tamanhoSenha;
-geraSenha();
-
-}
-function aumentaTamanho() {
-    if (tamanhoSenha < 20) {
-        // tamanhoSenha = tamanhoSenha+1;
-        tamanhoSenha++;
-    }
-    numeroSenha.textContent = tamanhoSenha;
-    geraSenha();
 }
 
 const campoSenha = document.querySelector('#campo-senha');
@@ -42,4 +31,16 @@ function geraSenha() {
         senha = senha + letrasMaiusculas[numeroAleatorio];
     }
     campoSenha.value = senha;
+}
+const campoSenha = document.querySelector('#campo-senha');
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+geraSenha();
+function geraSenha() {
+let senha = '';
+for (let i = 0; i < tamanhoSenha; i++) {
+let numeroAleatorio = Math.random() * letrasMaiusculas.length;
+numeroAleatorio = Math.floor(numeroAleatorio);
+senha = senha + letrasMaiusculas[numeroAleatorio];
+}
+campoSenha.value = senha;
 }
